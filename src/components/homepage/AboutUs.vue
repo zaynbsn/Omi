1c63cc41efbf46a78de0c58ec3e20d4506ffb481
zaynbsn <nils.vainui@gmail.com>
2023-06-18 09:44:26 +0200

    product & about responsive

--- src/components/homepage/AboutUs.vue
+++ src/components/homepage/AboutUs.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="container-sm">
-      <div class="about-container d-flex flex-column justify-content-center align-items-center" style="margin-top: 128px;">
+      <div class="about-container d-flex flex-column justify-content-center align-items-center"  :style="!isMobile ? 'margin: 128px 0;' : 'margin: 64px 0;'">
         <div class="about-title" style="width: 80%;" >
-          <TitleGradient label="Qui sommes-nous ?" left class="mb-4"></TitleGradient>
+          <TitleGradient label="Qui sommes&#8209;nous&#160;?" :left="isMobile ? false : true" class="mb-4"></TitleGradient>
         </div>
 
-        <div class="about-content d-flex justify-content-center align-items-center">
+        <div class="about-content d-flex justify-content-center align-items-center" :class="isMobile ? 'flex-column' : ''">
           <div class="content p-5">
             <div class="texts-container mb-5">
               <div v-for="(text, index) in texts" :key="index" class="text">
@@ -13,10 +13,16 @@
               </div>
             </div>
 
-            <SecondaryButton label="Decouvrez-nous" class="mt-4" to="/product"></SecondaryButton>
+            <SecondaryButton v-if="!isMobile" label="Decouvrez-nous" class="mt-4" to="/product"></SecondaryButton>
+            
+            <div v-if="isMobile" class="image-mobile">
+              <img src="@/assets/img/aboutImage.png" alt="about image">
+            </div>
           </div>
-
-          <div class="image" style="margin-left: -128px">
+          
+          <SecondaryButton v-if="isMobile" label="Decouvrez-nous" class="mt-4" to="/product"></SecondaryButton>
+          
+          <div v-if="!isMobile" class="image" style="margin-left: -128px">
             <img src="@/assets/img/aboutImage.png" alt="about image">
           </div>
         </div>
@@ -28,10 +34,12 @@
 import { ref } from 'vue'
 import TitleGradient from '@/components/UI/titleGradient.vue'
 import SecondaryButton from '@/components/UI/SecondaryButton.vue'
+import windowWidthMixin from '@/mixins/windowWidthMixin'
 
 export default {
   name: 'AboutUs',
   components: { TitleGradient, SecondaryButton },
+  mixins: [ windowWidthMixin ],
   setup() {
     const texts = ref([
       "Nous sommes trois étudiants en design d'innovation numérique à l'École des Gobelins. ",
@@ -50,11 +58,26 @@ export default {
   background-color: var(--md-blue);
   border-radius: 32px;
   color: white;
+  max-width: 90vw;
 }
 .text{
   font-family: 'Montserrat', sans-serif;
   font-weight: 400;
   font-size: 14px;
   max-width: 70%;
+}
+@media screen and (max-width: 768px){
+  .text {
+    max-width: 100vw;
+    width: fit-content;
+  }
+}
+.image-mobile {
+  width:100%;
+  height: auto;
+}
+.image-mobile img {
+  width:100%;
+  height: auto;
 }
 </style>
--- src/components/homepage/Product.vue
+++ src/components/homepage/Product.vue
@@ -1,18 +1,31 @@
 <template>
   <section class="container">
-      <div class="product-container row p-5" style="margin-top: 128px;">
-        <div class="product-image col-6 d-flex align-items center justify-content-center px-5">
+      <div class="product-container row" :class="!isMobile ? 'px-5' : 'mt-5'" :style="!isMobile ? 'margin: 128px 0;' : 'margin-left: 10px;'">
+        <div v-if="!isMobile" class="product-image col-6 d-flex align-items center justify-content-center " >
           <img src="@/assets/img/baseImage.png" alt="omi product" style="object-fit: contain; margin: 0 2rem 0 2rem; max-width: 100%"  >
         </div>
 
-        <div class="product-content col-6 row">
+        <div v-if="!isMobile" class="product-content col-6 row">
           <TitleGradient label="Notre Produit" left class="mb-4"></TitleGradient>
-
+          
           <div v-for="(text, index) in texts" :key="index" class="text">
             <p>{{ text }}</p>
           </div>
           
           <SecondaryButton label="Voir toutes les fonctionnalitées" class="mt-4" to="/product"></SecondaryButton>
+        </div>
+        
+        <div v-if="isMobile" class="product-content row">
+          <TitleGradient label="Notre Produit" class="mb-3"></TitleGradient>
+
+          <div class="mobile-div" style="position: relative;">
+            <img src="@/assets/img/mobileProduct.png" alt="mobile product" style="width: 100%;" />
+            <div class="mobile-texts p-4" style="position: absolute; top: 12%; color: white;">
+              <div v-for="(text, index) in mobileTexts" :key="index" class="text">
+                <p>{{ text }}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
@@ -22,10 +35,12 @@
 import { ref } from 'vue'
 import TitleGradient from '@/components/UI/TitleGradient.vue'
 import SecondaryButton from '@/components/UI/SecondaryButton.vue'
+import windowWidthMixin from '@/mixins/windowWidthMixin'
 
 export default {
   name: 'Product',
   components: { TitleGradient, SecondaryButton },
+  mixins: [ windowWidthMixin ],
   setup() {
     const texts = ref([
       "Bienvenue dans l'ère de l'organisation intelligente. Découvrez Omi, une solution innovante conçue pour simplifier votre quotidien.",
@@ -35,8 +50,14 @@ export default {
       "Omi est conçu pour rendre votre quotidien plus fluide et organisé. Il vous assure que vos essentiels sont toujours là où vous en avez besoin. Il élimine le stress de la recherche de vos objets perdus et vous donne la tranquillité d'esprit pour commencer ou terminer votre journée.",
       "Embrassez la simplicité, embrassez l'organisation, embrassez le futur avec notre Vide Poche Connecté.",
     ])
+    const mobileTexts = ref([
+      "Embarquez dans l'ère de l'organisation intelligente avec Omi, notre solution innovante pour simplifier votre quotidien. ",
+      "Omi, plus qu'un outil fonctionnel, est un ajout esthétique à votre intérieur, combinant style et fonctionnalité avec son miroir intégré. Son étagère comporte trois compartiments dédiés à vos clés et portefeuille, chacun avec une LED signalant leur présence. ",
+      "Un boîtier intelligent, équipé de détection ultrasonique, est installé sur votre porte pour capter les mouvements. ",
+      "Omi rend votre quotidien plus fluide, assure la disponibilité de vos essentiels et élimine le stress de la recherche d'objets perdus. Avec Omi, embrassez une vie simplifiée, organisée et futuriste.",
+    ])
 
-    return {texts}
+    return {texts, mobileTexts}
   }
 }
 </script>
